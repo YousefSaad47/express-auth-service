51d1ea88a1d73994cfe88509b55105636d35e0de
Yousef Saadallah <youssefsaad299@gmail.com>
2025-08-16 13:41:34 +0300

    fix(format): include jsx to the parser plugins

--- src/lib/utils/email/templates/email-verification-template.tsx
+++ src/lib/utils/email/templates/email-verification-template.tsx
@@ -1,13 +1,13 @@
-import { CSSProperties } from "react";
 import {
-  Html,
-  Head,
-  Preview,
   Body,
   Container,
-  Text,
+  Head,
+  Html,
   Link,
+  Preview,
+  Text,
 } from "@react-email/components";
+import { CSSProperties } from "react";
 
 interface EmailVerificationProps {
   to: string;
